e6ccd270e514622d5587714f9ed91b50ea32665c
MK Olsen <marykathrynolsen@gmail.com>
2018-04-30 14:14:08 -0400

    changed on submit to on click for click event for get dimensions

--- designs.js
+++ designs.js
@@ -1,32 +1,31 @@
 var grid, row, cell;
 
-
 //makes a grid from the given dimensions
 grid = ("#pixelCanvas");
 // When size is submitted by the user, call makeGrid()
 function makeGrid(gridHeight,gridWidth) {
   //iterates over the height
-  for(let i = 0; i < gridHeight; i++){
+  for(i = 0; i < gridHeight; i++){
     //creates a row in the grid
-    row = grid.append('<tr>');
+    row = grid.append('<tr></tr>');
+  }
  	//iterates over the width
-  	for(let x = 0; x < gridWidth; x++){
-      //creates a cell in the row
-      cell = row.append('<td>');
-    }
+  for(x = 0; x < gridWidth; x++){
+    //creates a cell in the row
+    cell = row.append('<td></td>');
   }
   changeColor();
 }
 
 
 //runs the getDimensions function when the submit button is clicked.
-sizePicker = $('#sizePicker');
-sizePicker.on("submit", getDimensions);
+var sizePicker = $('#sizePicker');
+sizePicker.on("click", getDimensions);
 //function gets the dimensions that were inputted by the user and puts them into the makeGrid function
 function getDimensions(event) {
     event.preventDefault();
-    var gridHeight = $("#inputHeight").val;
-   	var gridWidth = $("#inputWidth").val;
+    var gridHeight = $("#inputHeight").val();
+   	var gridWidth = $("#inputWidth").val();
 	  makeGrid(gridHeight,gridWidth);
 }
 
