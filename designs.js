99d52acbc6976c47e2268c0b5178112f7009efc7
MK Olsen <marykathrynolsen@gmail.com>
2018-04-30 14:39:55 -0400

    updated index to have jquery link, edited a few syntax errors in js

--- designs.js
+++ designs.js
@@ -1,7 +1,7 @@
 var grid, row, cell;
 
 //makes a grid from the given dimensions
-grid = ("#pixelCanvas");
+grid = $('#pixelCanvas');
 // When size is submitted by the user, call makeGrid()
 function makeGrid(gridHeight,gridWidth) {
   //iterates over the height
@@ -20,10 +20,10 @@ function makeGrid(gridHeight,gridWidth) {
 
 //runs the getDimensions function when the submit button is clicked.
 var sizePicker = $('#sizePicker');
-sizePicker.on("click", getDimensions);
+sizePicker.submit(getDimensions);
 //function gets the dimensions that were inputted by the user and puts them into the makeGrid function
-function getDimensions(event) {
-    event.preventDefault();
+function getDimensions(e) {
+    e.preventDefault();
     var gridHeight = $("#inputHeight").val();
    	var gridWidth = $("#inputWidth").val();
 	  makeGrid(gridHeight,gridWidth);
@@ -34,15 +34,16 @@ function getDimensions(event) {
 function changeColor() {
   var cells = $('td');
   cells.each(function(){
-    $(this).on( 'click', function( evt ) {
-        $( evt.target ).css( 'background', newColor );
+    $(this).click(function(e) {
+        $(e.target).css('background', newColor);
+      });
     });
-  })
+  }
 
 
 //selects the new color
-var colorBox = $("#colorPicker");
+var colorBox = $('#colorPicker');
 var newColor = colorBox.val();
-$colorBox.on("input", function() {
+colorBox.on("input", function() {
   newColor = colorBox.val();
   }, false);
